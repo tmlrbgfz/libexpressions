8d303003fa08c5800b55a38e4ed01d689b1ecc3c
Niklas Krafczyk <niklas@uni-bremen.de>
2022-04-11 18:36:48 +0200

    Moved function definitions to source file

---
+++ utils/expression-tree-visit.cpp
@@ -0,0 +1,25 @@
+#include "libexpressions/utils/expression-tree-visit.hpp"
+
+#include <iterator>
+#include <gsl/gsl_assert>
+
+namespace libexpressions {
+
+template<>
+size_t getChildNodeIndex(libexpressions::ExpressionNodePtr const *parent, libexpressions::ExpressionNodePtr const *child) {
+    libexpressions::Operator const *op = llvm::dyn_cast<libexpressions::Operator const>(parent->get());
+    Expects(op != nullptr); // ptr is a child of previous node. Thus, previous node must have children. Thus, previous node must be an operator.
+    auto iter = std::find(op->begin(), op->end(), *child);
+    Expects(op->end() != iter); // We expect to find some child equal to ptr
+    return static_cast<size_t>(std::distance(op->begin(), iter));
+}
+
+std::tuple<libexpressions::Operator::Iterator, libexpressions::Operator::Iterator> getChildrenIteratorsForExpressionNode(libexpressions::ExpressionNodePtr const &nodePtr) {
+    if( auto ptr = llvm::dyn_cast<libexpressions::Operator const>(nodePtr.get()); ptr != nullptr ) {
+        return std::make_tuple(ptr->begin(), ptr->end());
+    }
+    return std::make_tuple(libexpressions::Operator::Iterator(), libexpressions::Operator::Iterator());
+}
+
+}
+
--- utils/expression-tree-visit.hpp
+++ utils/expression-tree-visit.hpp
@@ -22,24 +22,14 @@
  */
 #pragma once
 
-#include <iterator>
-
 #include "libexpressions/utils/tree-visit.hpp"
 #include "libexpressions/expressions/operator.hpp"
 
-template<>
-size_t getChildNodeIndex(libexpressions::ExpressionNodePtr const *parent, libexpressions::ExpressionNodePtr const *child) {
-    libexpressions::Operator const *op = llvm::dyn_cast<libexpressions::Operator const>(parent->get());
-    Expects(op != nullptr); // ptr is a child of previous node. Thus, previous node must have children. Thus, previous node must be an operator.
-    auto iter = std::find(op->begin(), op->end(), *child);
-    Expects(op->end() != iter); // We expect to find some child equal to ptr
-    return static_cast<size_t>(std::distance(op->begin(), iter));
-}
+namespace libexpressions {
 
-std::tuple<libexpressions::Operator::Iterator, libexpressions::Operator::Iterator> getChildrenIteratorsForExpressionNode(libexpressions::ExpressionNodePtr const &nodePtr) {
-    if( auto ptr = llvm::dyn_cast<libexpressions::Operator const>(nodePtr.get()); ptr != nullptr ) {
-        return std::make_tuple(ptr->begin(), ptr->end());
-    }
-    return std::make_tuple(libexpressions::Operator::Iterator(), libexpressions::Operator::Iterator());
+template<>
+size_t getChildNodeIndex(libexpressions::ExpressionNodePtr const *parent, libexpressions::ExpressionNodePtr const *child);
+std::tuple<libexpressions::Operator::Iterator, libexpressions::Operator::Iterator> getChildrenIteratorsForExpressionNode(libexpressions::ExpressionNodePtr const &nodePtr);
+
 }
 
