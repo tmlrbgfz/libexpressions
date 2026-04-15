cbac3bf1cdaea2a6879a6a9f125c49b93e0c6f1b
Niklas Krafczyk <krafczyk.n@gmail.com>
2026-04-15 10:45:12 +0200

    Use old bison command line arguments for compatibility

--- representation_conversion/s-expressions/generate.sh
+++ representation_conversion/s-expressions/generate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 flex --header-file=s-expression-parser.lex.hpp --outfile=s-expression-parser.lex.cpp s-expression-parser.l
-bison -Hs-expression-parser.tab.hpp -o s-expression-parser.tab.cpp s-expression-parser.y
+bison --defines=s-expression-parser.tab.hpp -o s-expression-parser.tab.cpp s-expression-parser.y
